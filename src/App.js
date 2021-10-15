7efe6697436122fc4a2152257b6d1aeeb97fb101
lonewolf2580 <linusngwee@yahoo.com>
2021-10-15 11:59:25 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Ninjas name="Ryu" age="25" belt="Black" />
-      <Ninjas name="Yoshi" age="30" belt="White" />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 30, belt: 'white', id: 2 },
+      { name: 'Crystal', age: 30, belt: 'red', id: 3 },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
